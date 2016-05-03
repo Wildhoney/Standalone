1e34aa3e398c7b87fea3fbb735f2ce04f7b79879
Wildhoney <adam.timberlake@gmail.com>
2016-05-03 22:26:31 +0100

    Build

--- dist/standalone.js
+++ dist/standalone.js
@@ -54,6 +54,8 @@ module.exports =
 
 	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 	var _react = __webpack_require__(1);
@@ -205,7 +207,7 @@ module.exports =
 	        this.component = null;
 	    };
 
-	    Object.keys(methods).forEach(function (key) {
+	    (typeof methods === 'undefined' ? 'undefined' : _typeof(methods)) === 'object' && Object.keys(methods).forEach(function (key) {
 
 	        // Apply the user-defined functions onto the prototype.
 	        prototype[key] = prototype[key] || methods[key];
